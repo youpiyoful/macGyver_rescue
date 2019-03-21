cd579c5dd74d047023c070596063b873f9dea03d
YOAN FORNARI <yoan@vitavinum.com>
2019-03-21 15:18:06 +0100

    add move perso with  directional arrows

--- labyrinthe.py
+++ labyrinthe.py
@@ -3,17 +3,40 @@ from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((640, 480), RESIZABLE)
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((640, 480))
 
+#Chargement et collage du fond
 fond = pygame.image.load("background.jpg").convert()
 fenetre.blit(fond, (0,0))
 
+#Chargement et collage du personnage
 perso = pygame.image.load("perso.png").convert_alpha()
-fenetre.blit(perso, (200,300))
+position_perso = perso.get_rect()
+fenetre.blit(perso, position_perso)
 
+#Rafraîchissement de l'écran
 pygame.display.flip()
 
+#BOUCLE INFINIE
 continuer = 1
-
 while continuer:
-    continuer = int(input())
+	for event in pygame.event.get():	#Attente des événements
+		if event.type == QUIT:
+			continuer = 0
+		if event.type == KEYDOWN:
+			if event.key == K_DOWN:	#Si "flèche bas"
+				#On descend le perso
+				position_perso = position_perso.move(0,3)
+			if event.key == K_UP:
+				position_perso = position_perso.move(0,-3)
+			if event.key == K_LEFT:
+				position_perso = position_perso.move(-3,0)
+			if event.key == K_RIGHT:
+				position_perso = position_perso.move(3,0)
+	
+	#Re-collage
+	fenetre.blit(fond, (0,0))	
+	fenetre.blit(perso, position_perso)
+	#Rafraichissement
+	pygame.display.flip()

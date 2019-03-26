a58cb4cb14a4c51b4d3f02ead5998ac0f43b76a6
YOAN FORNARI <yoan@vitavinum.com>
2019-03-26 14:32:00 +0100

    en cours de refactoring vers de la poo

--- labyrinthe.py
+++ labyrinthe.py
@@ -3,22 +3,32 @@ from pygame.locals import *
 
 pygame.init()
 
-#Ouverture de la fenêtre Pygame
-fenetre = pygame.display.set_mode((640, 480))
 
-#Chargement et collage du fond
-fond = pygame.image.load("background.jpg").convert()
-fenetre.blit(fond, (0,0))
 
-#Chargement et collage du personnage
+class Map:
+	def openWindow(self, large, long):
+		self.window = pygame.display.set_mode((large, long))
+	def loadBackground(self, backgroundImage):
+		self.background = pygame.image.load(backgroundImage).convert()
+	def stickBackground(self, background):
+
+#Open window Pygame
+#window = pygame.display.set_mode((640, 480))
+
+#Load background
+background = pygame.image.load("background.jpg").convert()
+window.blit(background, (0,0))
+
+#Load perso
 perso = pygame.image.load("perso.png").convert_alpha()
 position_perso = perso.get_rect()
-fenetre.blit(perso, position_perso)
+window.blit(perso, position_perso)
 
-#Rafraîchissement de l'écran
-pygame.display.flip()
+#Screen refresh
+def screenRefresh():
+	pygame.display.flip()
 
-#BOUCLE INFINIE
+#Infinite loop
 continuer = 1
 while continuer:
 	for event in pygame.event.get():	#Attente des événements
@@ -35,8 +45,8 @@ while continuer:
 			if event.key == K_RIGHT:
 				position_perso = position_perso.move(3,0)
 	
-	#Re-collage
-	fenetre.blit(fond, (0,0))	
-	fenetre.blit(perso, position_perso)
-	#Rafraichissement
+	#Re-sticking
+	window.blit(background, (0,0))	
+	window.blit(perso, position_perso)
+	#Refresh
 	pygame.display.flip()

a1b585072a64eb9fce4ec833f7e9687769332585
YOAN FORNARI <yoan@vitavinum.com>
2019-04-08 14:12:32 +0200

    rename labyrinthe to app and create a new labyrinthe.py, i create a map too with 0, 1, 2 number for map case

---
+++ app.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+
+# class Map:
+# 	def openWindow(self, large, long):
+# 		self.window = pygame.display.set_mode((large, long))
+# 	def loadBackground(self, backgroundImage):
+# 		self.background = pygame.image.load(backgroundImage).convert()
+# 	def stickBackground(self, background):
+
+#Open window Pygame
+window = pygame.display.set_mode((640, 480))
+
+#Load background
+background = pygame.image.load("background.jpg").convert()
+window.blit(background, (0,0))
+
+#Load perso
+perso = pygame.image.load("perso.png").convert_alpha()
+position_perso = perso.get_rect()
+window.blit(perso, position_perso)
+
+#Screen refresh
+def screenRefresh():
+	pygame.display.flip()
+
+#Infinite loop
+continuer = 1
+while continuer:
+	for event in pygame.event.get():	#Attente des événements
+		if event.type == QUIT:
+			continuer = 0
+		if event.type == KEYDOWN:
+			if event.key == K_DOWN:	#Si "flèche bas"
+				#On descend le perso
+				position_perso = position_perso.move(0,3)
+			if event.key == K_UP:
+				position_perso = position_perso.move(0,-3)
+			if event.key == K_LEFT:
+				position_perso = position_perso.move(-3,0)
+			if event.key == K_RIGHT:
+				position_perso = position_perso.move(3,0)
+	
+	#Re-sticking
+	window.blit(background, (0,0))	
+	window.blit(perso, position_perso)
+	#Refresh
+	pygame.display.flip()
--- labyrinthe.py
+++ labyrinthe.py
@@ -1,52 +1,23 @@
-import pygame
-from pygame.locals import *
-
-pygame.init()
-
-
-
-class Map:
-	def openWindow(self, large, long):
-		self.window = pygame.display.set_mode((large, long))
-	def loadBackground(self, backgroundImage):
-		self.background = pygame.image.load(backgroundImage).convert()
-	def stickBackground(self, background):
-
-#Open window Pygame
-#window = pygame.display.set_mode((640, 480))
-
-#Load background
-background = pygame.image.load("background.jpg").convert()
-window.blit(background, (0,0))
-
-#Load perso
-perso = pygame.image.load("perso.png").convert_alpha()
-position_perso = perso.get_rect()
-window.blit(perso, position_perso)
-
-#Screen refresh
-def screenRefresh():
-	pygame.display.flip()
-
-#Infinite loop
-continuer = 1
-while continuer:
-	for event in pygame.event.get():	#Attente des événements
-		if event.type == QUIT:
-			continuer = 0
-		if event.type == KEYDOWN:
-			if event.key == K_DOWN:	#Si "flèche bas"
-				#On descend le perso
-				position_perso = position_perso.move(0,3)
-			if event.key == K_UP:
-				position_perso = position_perso.move(0,-3)
-			if event.key == K_LEFT:
-				position_perso = position_perso.move(-3,0)
-			if event.key == K_RIGHT:
-				position_perso = position_perso.move(3,0)
-	
-	#Re-sticking
-	window.blit(background, (0,0))	
-	window.blit(perso, position_perso)
-	#Refresh
-	pygame.display.flip()
+ map = [
+#  0  1  2  3  4   5  6  7  8  9  0  1  2  3  4  5  6  7  8  9--
+	[0, 0, 0, 0, 1 , 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0], # 0 
+	[0, 0, 0, 1, 1, 1, 1, 0, 0 , 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0], # 1
+	[0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1 , 1, 1, 1, 0, 0, 0, 0], # 2
+	[0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0 , 0, 0, 0], # 3
+	[0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],  # 4
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 5
+	[0, 0, 0, 0 , 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0], # 6
+	[0, 0, 0, 0, 0, 2, 2, 2 , 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0], # 7
+	[0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2 , 2, 2, 2, 0, 0, 0, 0, 0], # 8
+	[0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 , 0, 0, 0, 0], # 9
+	[0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0 ], # 10
+	[0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0],  # 11
+	[0, 0, 0 , 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0], # 12
+	[0, 0, 0, 0, 0, 0, 2 , 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0], # 13
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2 , 0, 0, 0, 0, 0, 0, 0, 0, 0], # 14
+	[0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 15
+	[0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 16
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], # 17
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0], # 18
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0], # 19
+] 

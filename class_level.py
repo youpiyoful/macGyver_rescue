f0ba647febed9a94ece357ff7df79fe13f5f336d
YOAN FORNARI <yoan@vitavinum.com>
2019-06-28 20:17:30 +0200

    [refactoring] j'ai utilisé pylint pour tester mes fichier est atteindre un score de 8/10

--- class_level.py
+++ class_level.py
@@ -13,83 +13,83 @@ from constantes import WALL_IMAGE
 
 
 class Level:
-	"""Class for create level"""
+    """Class for create level"""
 
-	def __init__(self, file):
-		self.file = file
-		self.structure = 0
+    def __init__(self, file):
+        self.file = file
+        self.structure = 0
 
-	def generate(self):
-		"""Method for generate level by file.
-		We create a general list, who contain a list by line to display"""
-		# we open file
-		with open(self.file, "r") as file:
-			structure_level = []
-			# we rune line of file
-			for line in file:
-				line_level = []
-				# we run sprites (letters) contain in file
-				for sprite in line:
-					# we ignore the "\n" of end line
-					if sprite != '\n':
-						# we add the sprite at line of level
-						line_level.append(sprite)
-				# we add the line at structure level
-				structure_level.append(line_level)
-			# we save structure
-			self.structure = structure_level
+    def generate(self):
+        """Method for generate level by file.
+        We create a general list, who contain a list by line to display"""
+        # we open file
+        with open(self.file, "r") as file:
+            structure_level = []
+            # we rune line of file
+            for line in file:
+                line_level = []
+                # we run sprites (letters) contain in file
+                for sprite in line:
+                    # we ignore the "\n" of end line
+                    if sprite != '\n':
+                        # we add the sprite at line of level
+                        line_level.append(sprite)
+                # we add the line at structure level
+                structure_level.append(line_level)
+            # we save structure
+            self.structure = structure_level
 
-	def random_obj(self):
-		objects = ['E', 'T', 'N']  # init list of objects
-		for obj in objects:  # instruction for each object
-			x = 0  # init value
-			y = 0  # init value
-			self.structure[x][y] = None  # init != 0 for allow to enter in loop
-			while self.structure[x][y] != '0':
-				line = random.randint(0, 14)
-				case = random.randint(0, 14)
-				x = line  # TODO faire en sorte que les objets n'apparaissent pas au même endroit
-				y = case
-			self.structure[x][y] = obj
+    def random_obj(self):
+        objects = ['E', 'T', 'N']  # init list of objects
+        for obj in objects:  # instruction for each object
+            x = 0  # init value
+            y = 0  # init value
+            self.structure[x][y] = None  # init != 0 for allow to enter in loop
+            while self.structure[x][y] != '0':
+                line = random.randint(0, 14)
+                case = random.randint(0, 14)
+                x = line  # TODO faire en sorte que les objets n'apparaissent pas au même endroit
+                y = case
+            self.structure[x][y] = obj
 
-	def display(self, window):
-		"""Method for display level by list of structure output by generate()"""
-		# Loading images
-		wall = pygame.image.load(WALL_IMAGE).convert()
-		start = pygame.image.load(START_IMAGE).convert()
-		end = pygame.image.load(ARRIVAL_IMAGE).convert_alpha()
-		tube = pygame.image.load(TUBE).convert_alpha()
-		needle = pygame.image.load(NEEDLE).convert_alpha()
-		ether = pygame.image.load(ETHER).convert_alpha()
+    def display(self, window):
+        """Method for display level by list of structure output by generate()"""
+        # Loading images
+        wall = pygame.image.load(WALL_IMAGE).convert()
+        start = pygame.image.load(START_IMAGE).convert()
+        end = pygame.image.load(ARRIVAL_IMAGE).convert_alpha()
+        tube = pygame.image.load(TUBE).convert_alpha()
+        needle = pygame.image.load(NEEDLE).convert_alpha()
+        ether = pygame.image.load(ETHER).convert_alpha()
 
-		# we run the level's list
-		num_line = 0
+        # we run the level's list
+        num_line = 0
 
-		for line in self.structure:
-			# we run list of lines
-			num_case = 0
+        for line in self.structure:
+            # we run list of lines
+            num_case = 0
 
-			for sprite in line:
-				# we calcul the real position in pixels
-				x = num_case * SPRITE_SIZE
-				y = num_line * SPRITE_SIZE
+            for sprite in line:
+                # we calcul the real position in pixels
+                x = num_case * SPRITE_SIZE
+                y = num_line * SPRITE_SIZE
 
-				if sprite == 'w':  # m = wall
-					window.blit(wall, (x, y))
+                if sprite == 'w':  # m = wall
+                    window.blit(wall, (x, y))
 
-				elif sprite == 's':  # s = start
-					window.blit(start, (x, y))
+                elif sprite == 's':  # s = start
+                    window.blit(start, (x, y))
 
-				elif sprite == 'e':  # e = end
-					window.blit(end, (x, y))
+                elif sprite == 'e':  # e = end
+                    window.blit(end, (x, y))
 
-				elif sprite == 'E':
-					window.blit(ether, (x, y))
+                elif sprite == 'E':
+                    window.blit(ether, (x, y))
 
-				elif sprite == 'N':
-					window.blit(needle, (x, y))
+                elif sprite == 'N':
+                    window.blit(needle, (x, y))
 
-				elif sprite == 'T':
-					window.blit(tube, (x, y))
-				num_case += 1
-			num_line += 1
+                elif sprite == 'T':
+                    window.blit(tube, (x, y))
+                num_case += 1
+            num_line += 1
--- main.py
+++ main.py
@@ -7,10 +7,9 @@ Script Python
 Files : main.py, classes.py, constantes.py, l1, l2 + images
 """
 import pygame
-
 from pygame.constants import KEYDOWN
-from pygame.constants import K_ESCAPE
 from pygame.constants import K_DOWN
+from pygame.constants import K_ESCAPE
 from pygame.constants import K_F1
 from pygame.constants import K_F2
 from pygame.constants import K_LEFT
@@ -18,8 +17,6 @@ from pygame.constants import K_RIGHT
 from pygame.constants import K_UP
 from pygame.constants import QUIT
 
-# from classes import Character
-# from classes import Level
 from class_character import Character
 from class_level import Level
 from constantes import BACKGROUND_IMAGE
@@ -28,12 +25,12 @@ from constantes import ICONE_IMAGE
 from constantes import IMAGE_CHARACTER
 from constantes import LOST
 from constantes import WIN
-from constantes import WINDOW_TITLE
 from constantes import WINDOW_SIDE
+from constantes import WINDOW_TITLE
 
 
 def initialize_window():
-    pygame.init()
+    """ initialize window with pygame function """
     # Open window Pygame
     window = pygame.display.set_mode((WINDOW_SIDE, WINDOW_SIDE))
     # Icone
@@ -45,19 +42,23 @@ def initialize_window():
 
 
 def loading_home_page(image, window):
+    """ load the homepage """
     home = pygame.image.load(image).convert()
     window.blit(home, (0, 0))
 
 
 def display_new_position(background, level, character, window):
+    """ display new position of character """
     # Displays at new positions
     window.blit(background, (0, 0))  # Reload background with good position
     level.display(window)  # Reload the element of the map at the good position
-    window.blit(character.direction, (character.x, character.y)) # Reload character at the good position in the map
+    # Reload character at the good position in the map
+    window.blit(character.direction, (character.x, character.y))
     pygame.display.flip()  # Update the "surface"
 
 
 def end_game(character, window):
+    """ verify the score for return win or lose """
     win = 1
     lost = 0
 
@@ -72,17 +73,19 @@ def end_game(character, window):
         image_lost = pygame.image.load(LOST).convert_alpha()
         window.blit(image_lost, (0, 0))
         result = lost
-            
+
     pygame.display.flip()
     return result
 
 
 def initialize_background():
+    """ initialise the background of map game """
     background = pygame.image.load(BACKGROUND_IMAGE).convert_alpha()  # Loading background
     return background
 
 
 def init_level(choice, window):
+    """ initialize the map with the player choice """
     # Generate a choice from a FILE
     level = Level(choice)
     level.generate()
@@ -92,9 +95,13 @@ def init_level(choice, window):
 
 
 def score_meter(position, score, quest_item_list):
+    """
+        add + 1 to variable score when the position
+        of character is on an object
+    """
     if position == 'N' and 'N' not in quest_item_list:
         score += 1
-    
+
     elif position == 'T' and 'T' not in quest_item_list:
         score += 1
 
@@ -105,12 +112,14 @@ def score_meter(position, score, quest_item_list):
 
 
 def transform_object_in_empty_case(position):
-    if position == 'E' or position == 'N' or position == 'T':
+    """ replace sign object by 0 when player position is on object"""
+    if position in ('E', 'N', 'T'):
         position = '0'
     return position
 
 
 def stock_quest_item(position, quest_item_list):
+    """ record and return the list of taken object """
 
     if position == 'N' and 'N' not in quest_item_list:
         quest_item_list.append('N')
@@ -120,11 +129,12 @@ def stock_quest_item(position, quest_item_list):
 
     elif position == 'E' and 'E' not in quest_item_list:
         quest_item_list.append('E')
-    
+
     return quest_item_list
 
 
 def main():
+    """ main function who run 3 loops and call all the function"""
     window = initialize_window()
     carry_on = 1  # Start the loop
 
@@ -140,27 +150,27 @@ def main():
         # we remake variables to 1 for each looping
         carry_on_game = 1
         carry_on_home = 1
-        
+
         # MENU LOOP
         while carry_on_home:
             # Limit of loop speed
             pygame.time.Clock().tick(30)
-        
+
             for event in pygame.event.get():
-            
+
                 # If user leave, we make variables to loop at 0 for don't launch anything and exit
                 if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
                     carry_on_home = 0
                     carry_on_game = 0
                     carry_on = 0
                     choice = 0  # Variable for choice level
-                    
+
                 elif event.type == KEYDOWN:
                     # Launch choice 1
                     if event.key == K_F1:
                         carry_on_home = 0  # Leave home
                         choice = 'l1'  # Map choice
-                        
+
                     # launch to choice 2
                     elif event.key == K_F2:
                         carry_on_home = 0
@@ -170,62 +180,64 @@ def main():
         if choice != 0:
             level = init_level(choice, window)
             background = initialize_background()
-            mc = Character(IMAGE_CHARACTER, level)  # Creation of mc Gyver
-                    
+            mac = Character(IMAGE_CHARACTER, level)  # Creation of mac Gyver
+
         # GAME LOOP
         while carry_on_game:
             # Limit speed looping
             pygame.time.Clock().tick(30)
-        
+
             for event in pygame.event.get():
-            
+
                 # If user leave, variable who continue the game = 0 for close the window
                 if event.type == QUIT:
                     carry_on_game = 0
                     carry_on = 0
-            
+
                 elif event.type == KEYDOWN:
 
                     # If user push escape here, we comeback only at home
                     if event.key == K_ESCAPE:
                         carry_on_game = 0
-                        
+
                     # Keyboard of moove to mc_gyver
                     elif event.key == K_RIGHT:
-                        mc.moove('right')
+                        mac.moove('right')
 
                     elif event.key == K_LEFT:
-                        mc.moove('left')
+                        mac.moove('left')
 
                     elif event.key == K_UP:
-                        mc.moove('up')
+                        mac.moove('up')
 
                     elif event.key == K_DOWN:
-                        mc.moove('down')			
-            
-            display_new_position(background, level, mc, window)
-            position = level.structure[mc.case_y][mc.case_x]  # Store position in a variable named position
-            mc_gyver_score = score_meter(position, mc_gyver_score, quest_item_list)  # calcul and return score
-            quest_item_list = stock_quest_item(position, quest_item_list)  # Insert the object into a list
+                        mac.moove('down')
+
+            display_new_position(background, level, mac, window)
+            # Store position in a variable named position
+            position = level.structure[mac.case_y][mac.case_x]
+            # calcul and return score
+            mc_gyver_score = score_meter(position, mc_gyver_score, quest_item_list)
+            # Insert the object into a list
+            quest_item_list = stock_quest_item(position, quest_item_list)
 
             # delete the object pick up by mc_gyer of the map
-            level.structure[mc.case_y][mc.case_x] = transform_object_in_empty_case(position)
+            level.structure[mac.case_y][mac.case_x] = transform_object_in_empty_case(position)
 
             # verify if mc_gyver is on the end case
-            if level.structure[mc.case_y][mc.case_x] == 'e':  # leave the game
+            if level.structure[mac.case_y][mac.case_x] == 'e':  # leave the game
 
                 # displays possessed objects
                 for item in quest_item_list:
                     print(item)
 
-                # displays result message 
+                # displays result message
                 while mc_gyver_score in [0, 1, 2, 3]:  # Allow to verify than mc_gyver is not None
                     print(mc_gyver_score)
                     end_game(mc_gyver_score, window)
-                    
-                    # allows you to leave the result image of the game
-                    for event in pygame.event.get():
-                        
+
+                    for event in pygame.event.get():  # allows leave image result
+
                         if event.type == KEYDOWN:
                             carry_on_game = 0  # comeback in home loop
                             mc_gyver_score = None
